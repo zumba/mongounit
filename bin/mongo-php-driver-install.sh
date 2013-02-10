c84aa1fb8e2bc07a0a51f5b87b15a842d8767bbd
Chris Saylor <cjsaylor@gmail.com>
2013-02-10 02:21:31 -0500

    Updated travis mongodb config back to pecl and added mongo environments.

--- bin/mongo-php-driver-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- 
-wget http://pecl.php.net/get/mongo-1.2.10.tgz
-tar -xzf mongo-1.3.4.tgz
-sh -c "cd mongo-1.3.4 && phpize && ./configure && sudo make install"
-echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
